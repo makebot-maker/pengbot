888af84c59faf3227c8728fba10e90aae53a719f
WONDAEHAN <62107740+makebot-maker@users.noreply.github.com>
2020-11-25 17:20:40 +0900

    Update and rename pengbot_bt.ino to code/bluetooth.ino

---
+++ code/bluetooth.ino
@@ -0,0 +1,55 @@
+/*makebot pengbot library
+ * makebot homepage(English) : http://makebot.mystrikingly.com/
+ * makebot homepage(Korean) : https://makebot.modoo.at
+ * files : https://github.com/makebot-lab/pengbot
+ * made by WONDAEHAN.
+ */
+#include <Pengbot.h>
+#include <SoftwareSerial.h>
+
+Pengbot Pengbot;
+
+SoftwareSerial BTserial(0, 1);
+
+void setup() {
+  BTserial.begin(9600);
+  Pengbot.ready();
+  Pengbot.stop();
+}
+
+void loop() {
+  if(BTserial.available()) {
+    char btread = BTserial.read();
+
+    if(btread == 'A') {
+      Pengbot.stop();
+    }
+    if(btread == 'B') {
+      Pengbot.forward(45);
+    }
+    if(btread == 'C') {
+      Pengbot.turnleft(45);
+    }
+    if(btread == 'D') {
+      Pengbot.turnright(45);
+    }
+    if(btread == 'F') {
+      Pengbot.turnright(45);
+    }
+    if(btread == 'H') {
+      Pengbot.sound(6);
+    }
+    if(btread == 'I') {
+      Pengbot.sound(2);
+    }
+    if(btread == 'J') {
+      Pengbot.sound(1);
+    }
+    if(btread == 'K') {
+      Pengbot.sound(3);
+    }
+    if(btread == 'L') {
+      Pengbot.sound(5);
+    }
+  }
+}
