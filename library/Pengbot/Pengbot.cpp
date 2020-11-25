5784efd2866af99f9ed7a890a060934a4c2e76b3
WONDAEHAN <62107740+makebot-maker@users.noreply.github.com>
2020-11-25 15:39:56 +0900

    Update Pengbot.cpp

--- library/Pengbot/Pengbot.cpp
+++ library/Pengbot/Pengbot.cpp
@@ -24,7 +24,7 @@ float Pengbot::distance(){
   return us.read();
 }
 
-//Pengbot
+//pengbot
 void Pengbot::ready() {  //ready
   for(int x = 0; x < 2; x++) {
     attachservo(x);
@@ -32,5 +32,76 @@ void Pengbot::ready() {  //ready
   }
   noTone(13);
   us.init(A0, A1);
-  home();
+  stop();
+}
+
+void Pengbot::stop() {  //stop
+  
+}
+
+void Pengbot::forward(int speed) {  //forward
+  
+}
+
+void Pengbot::backward(int speed) {  //backward
+  
+}
+
+void Pengbot::sound(int soundnum) {  //sound
+  if(soundnum == 1) { //happy
+    tone(13, 1300);
+    delay(50);
+    tone(13, 1125);
+    delay(100);
+    tone(13, 1500);
+    delay(100);
+    tone(13, 1700);
+    delay(100);
+  }else if(soundnum == 2) { //sad
+    tone(13, 240);
+    delay(100);
+    noTone(13);
+    delay(500);
+    tone(13, 240);
+    delay(200);
+    noTone(13);
+    delay(350);
+    tone(13, 220);
+    delay(400);
+    noTone(13);
+    delay(350);
+    tone(13, 200);
+    delay(400);
+  }else if(soundnum == 3) { //surprised
+    tone(13, 5000);
+    delay(850);
+  }else if(soundnum == 4) { //angry
+    tone(13, 300);
+    delay(200);
+    tone(13, 100);
+    delay(400);
+  }else if(soundnum == 5) { //sleepy
+    tone(13, 100);
+    delay(800);
+    noTone(13);
+    delay(800);
+    tone(13, 100);
+    delay(800);
+    noTone(13);
+    delay(800);
+    tone(13, 100);
+    delay(800);
+    noTone(13);
+    delay(800);
+    tone(13, 100);
+    delay(800);
+  }else if(soundnum == 6) { //joy
+    tone(13, 1200);
+    delay(100);
+    tone(13, 1500);
+    delay(100);
+    tone(13, 1800);
+    delay(300);
+  }
+  noTone(13);
 }
